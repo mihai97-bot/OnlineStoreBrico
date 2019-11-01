caa2dd3b5127f2abba2ed9e662502e38950acf56
justroso <36778248+justroso@users.noreply.github.com>
2019-11-01 15:20:47 +0200

    Add project files.

---
+++ OnlineStoreBrico/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OnlineStoreBrico.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
